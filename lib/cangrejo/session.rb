b61a715338f4e6b76204547813cc33fff2d630a5
Ignacio Baixas <iobaixas@gmail.com>
2015-01-12 20:00:09 -0300

    feat(session): adds retry logic to session

--- lib/cangrejo/session.rb
+++ lib/cangrejo/session.rb
@@ -3,7 +3,9 @@ require "ostruct"
 module Cangrejo
   class Session
 
-    attr_reader :doc
+    WAIT_STEP = 45.0 # Ruby sockets automatically timeout after 60 secs
+
+    attr_reader :doc, :state_name, :state_params
 
     def initialize(_name, _options={})
       @name = _name
@@ -18,12 +20,8 @@ module Cangrejo
       self
     end
 
-    def state_name
-      @rest.name
-    end
-
-    def state_params
-      @rest.params
+    def elapsed
+      @rest.elapsed
     end
 
     def raw_doc
@@ -32,8 +30,16 @@ module Cangrejo
 
     def crawl(_state, _params={})
       raise ConfigurationError.new 'Session not started' if @rest.nil?
-      @rest.put(name: _state, params: _params)
-      @doc = OpenStruct.new @rest.doc
+
+      params = add_timestamp(_params)
+
+      while_times_out do
+        @rest.put(name: _state, params: params, wait: WAIT_STEP)
+        @state_name = _state
+        @state_params = _params
+      end
+
+      wrap_response_doc
       self
     end
 
@@ -44,8 +50,31 @@ module Cangrejo
 
   private
 
+    def add_timestamp(_params)
+      _params.merge({
+        '_ts' => DateTime.now.strftime('%Q')
+      })
+    end
+
+    def while_times_out
+      loop do
+        begin
+          yield
+          break
+        rescue RestClient::RequestTimeout
+          # retry on timeout
+        end
+      end
+    end
+
+    def wrap_response_doc
+      @doc = OpenStruct.new @rest.doc
+    end
+
     def select_mode(_options)
-      @mode = if _options.has_key? :path
+      @mode = if _options.has_key? :mode
+        _options[:mode]
+      elsif _options.has_key? :path
         require "cangrejo/modes/local"
         Modes::Local.new _options[:path]
       elsif _options.has_key? :git_remote
